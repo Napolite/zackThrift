a4d9208bcec4906ce4c9b35204ff02548be2b79c
constantine <okonemmanuelmma@gmail.com>
2024-02-25 21:38:49 +0100

    wireframe for landing

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import LandingPage from "../landing/landing";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home-root">
+      <div>
+        <LandingPage />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/landing/landing.tsx
@@ -0,0 +1,13 @@
+import "./landing.css";
+
+function LandingPage() {
+  return (
+    <div className="landing-root">
+      <div className="after"></div>
+      <div className="name">ZackLaunren Thrift Collection</div>
+      <div className="after"></div>
+    </div>
+  );
+}
+
+export default LandingPage;
