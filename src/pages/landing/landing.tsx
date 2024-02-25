9aa030d89a30d7e03325701324eaaeb9f6e4a274
constantine <okonemmanuelmma@gmail.com>
2024-02-25 22:32:46 +0100

    initial animation agreement

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -3,7 +3,7 @@ import "./landing.css";
 function LandingPage() {
   return (
     <div className="landing-root">
-      <div className="after"></div>
+      <div className="before"></div>
       <div className="name">ZackLaunren Thrift Collection</div>
       <div className="after"></div>
     </div>
