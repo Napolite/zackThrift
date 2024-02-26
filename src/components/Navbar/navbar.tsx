acd62f36fc3996d498d50d58f642fa576389b44e
constantine <okonemmanuelmma@gmail.com>
2024-02-26 13:58:59 +0100

    routing skeleton

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div className="">
+      <div>About</div>
+      <div>Collection</div>
+      <div>Contact</div>
+    </div>
+  );
+}
+
+export default Navbar;
